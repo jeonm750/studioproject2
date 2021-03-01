7a67956f452bca07d9340db16b08269912a856b2
jeonm750 <jeonm750@newschool.edu>
2021-03-01 11:43:51 -0500

    files

---
+++ path1/left.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+
+  $(".first").click(function(){
+    $(".first").replaceWith($(".second")); 
+  });
+
+$(".second").hide();
+$(".first").click(function(){
+    $(".second").show();       
+	});
+
+
+$(".second").click(function(){
+    $(".second").replaceWith($(".third")); 
+  });
+
+$(".third").hide();
+$(".second").click(function(){
+    $(".third").show();       
+	});
+
+$(".third").click(function(){
+    $(".third").replaceWith($(".fourth")); 
+  });
+
+$(".fourth").hide();
+$(".third").click(function(){
+    $(".fourth").show();       
+	});
+
+$(".fourth").click(function(){
+    $(".fourth").replaceWith($(".fifth")); 
+  });
+
+$(".fifth").hide();
+$(".fourth").click(function(){
+    $(".fifth").show();       
+	});
+
+$(".fifth").click(function(){
+    $(".fifth").replaceWith($(".sixth")); 
+  });
+
+$(".sixth").hide();
+$(".fifth").click(function(){
+    $(".sixth").show();       
+  });
+});
---
+++ script.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+   /* $("h1").delay(5000).fadeOut(1000); */
+  	$("h2").toggle().fadeIn(5000);
+  });
